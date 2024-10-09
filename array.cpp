7681cccc7448187e2534a6936272785d2d2503eb
Lazarus_W <your-username@user.noreply.github.com>
2024-10-09 07:15:07 -0300

    this is the second commit of my project

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+/*int odd[5];
+	int even[]={2,4,6,8,10};
+odd [0]=1;
+odd[1]=3;
+odd[2]=5;	
+odd[3]=7;
+cout<<odd[3];*/
+
+ int num[6]={1,2,3,4,5,6};
+ for(int i=6;i<6;i++){
+    cout<<"number of array:"<<i<<endl;
+	}
+
+	return 0;
+}
